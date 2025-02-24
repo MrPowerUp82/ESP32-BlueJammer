52522e40ec2e646aa2ae607106e823ddef18d589
MrPowerUp82 <gustavohenrique8282@hotmail.com>
2025-02-24 08:06:25 -0300

    Adiciona exemplos de jammers para RF24 com suporte a OLED e controle de canais

--- examples/jammer_wokwi.ino
+++ examples/jammer_wokwi.ino
@@ -13,10 +13,6 @@ RF24 radio(7, 8); // CE, CSN
 const int startChannel = 10; // Start of the range
 const int endChannel = 30;   // End of the range
 
-int bluetooth_channels[] = {32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26, 28, 30, 74, 76, 78, 80};
-int ble_channels[] = {2, 26, 80};
-
-
 void setup() {
   Serial.begin(57600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -26,20 +22,6 @@ void setup() {
   radio.setDataRate(RF24_2MBPS);  // Fastest data rate
   display.setTextSize(1);
   display.setTextColor(WHITE);
-}
-
-void configureRadio(RF24 &radio, int channel, SPIClass *spi) {
-  if (radio.begin(spi)) {
-      radio.setAutoAck(false);
-      radio.stopListening();
-      radio.setPALevel(RF24_PA_MAX, true);
-      radio.startConstCarrier(RF24_PA_HIGH, channel);
-  }
-}
-
-void jamBLE() {
-  int randomIndex = random(0, sizeof(ble_channels) / sizeof(ble_channels[0]));
-  radio.setChannel(ble_channels[randomIndex]);
 }
 
 void jammer() {
@@ -67,5 +49,3 @@ void jammer() {
 void loop() {
   jammer();
 }
-
-
